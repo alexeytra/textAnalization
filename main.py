cae4ec67922a4fc0d060ee7033d0cfe7f85fa07f
alex <alekceytr@mail.ru>
2019-04-20 15:34:44 +0800

    fix get information and layers

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 import sklearn
 import numpy
 import voсabulary as voc
-from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from keras.models import Sequential
 from keras import layers
 import os
+from keras.preprocessing.text import Tokenizer
+from sklearn.preprocessing import LabelBinarizer
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-sentaces_train, action_train, sentaces_test, action_test = voc.getCSVSample()
+sentences_train, action_train, sentences_test, action_test = voc.getCSVSample()
 
-vocabulary = CountVectorizer()
-vocabulary.fit(sentaces_train)
-X_train = vocabulary.transform(sentaces_train)
-vocabulary.fit(action_train)
-y_train = vocabulary.transform(sentaces_train)
 
-input_dim = X_train.shape[1]
+#
+# vocabulary = CountVectorizer()
+# vocabulary.fit(sentences_train)
+# X_train = vocabulary.transform(sentences_train)
+# print(X_train)
+#
+# input_dim = X_train.shape[1]
+# model = Sequential()
+# model.add(layers.Dense(13, input_dim=input_dim, activation='relu'))
+# model.add(layers.Dense(1, activation='sigmoid'))
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.summary()
+# history = model.fit(X_train, y_train, epochs=100, verbose=False, batch_size=5)
+#
+#
+# vocabulary.fit(sentences_train)
+# sentence = vocabulary.transform(voc.getCSVTest())
+# print(model.predict_classes(sentence))
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_matrix(sentences_train, mode='tfidf')
+X_test = tokenizer.texts_to_matrix(sentences_test, mode='tfidf')
+
+print(action_train)
+encoder = LabelBinarizer()
+encoder.fit(action_train)
+y_train = encoder.transform(action_train)
+y_test = encoder.transform(action_test)
+
+
 model = Sequential()
-model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
-model.add(layers.Dense(y_train.shape[1], activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(layers.Dense(512, input_shape=(X_train.shape[1],)))
+model.add(layers.Activation('relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(512))
+model.add(layers.Activation('relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(3))
+model.add(layers.Activation('softmax'))
 model.summary()
-history = model.fit(X_train, y_train, epochs=100, verbose=False, batch_size=10)
 
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-vocabulary.fit(sentaces_train)
-sentence = vocabulary.transform(voc.getCSVTest())
-print(model.predict(sentence))
+history = model.fit(X_train, y_train, batch_size=5, epochs=40, verbose=1, validation_split=0.1)
+
+score = model.evaluate(X_test, y_test, batch_size=2, verbose=1)
+print('Test accuracy:', score[1])
--- voсabulary.py
+++ voсabulary.py
@@ -1,20 +1,19 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+import numpy
 
 def getCSVSample():
     df_list = []
     table = pd.read_csv('sample.csv', names=['x', 'r', 'y', 'action'])
     df_list.append(table)
     df = pd.concat(df_list)
-
-    sentences = df['x'].values + " " + df['r'].values + " " + df['y'].values
-    action = df['action'].values
+    df['sentence'] = df['x'] + ' ' + df['r'] + ' ' + df['y']
+    sentences = df['sentence']
+    action = df['action']
 
     sentences_train, sentences_test, action_train, action_test = train_test_split(sentences, action, test_size=0.5, random_state=1000)
     return sentences_train, action_train, sentences_test, action_test
-    # vocabulary = CountVectorizer(min_df=0, lowercase=False)
-    # vocabulary.fit(sentences)
 
 def getCSVTest():
     df_list = []
